77b02923344355d5b649e43572c68a7e2448f6d0
kaitorisenkou <kaitorisenkou@icloud.com>
2024-05-21 01:44:39 +0900

    プロジェクト ファイルを追加します。

---
+++ RemoveImplantForcibly/Recipe_RemoveImplantForcibly.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+
+namespace RemoveImplantForcibly {
+    public class Recipe_RemoveImplantForcibly : Recipe_RemoveImplant {
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill) {
+            base.ApplyOnPawn(pawn, part, billDoer, ingredients, bill);
+            pawn.Kill(
+                new DamageInfo(
+                    DamageDefOf.SurgicalCut,
+                    99999f, 
+                    999f, 
+                    hitPart:part 
+                    )
+                );
+        }
+    }
+}
